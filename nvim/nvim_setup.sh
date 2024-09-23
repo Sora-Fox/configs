ca53e1cb93a0103bfceb81a51df8b2ec8566a258
Fox <dhhugtu2@gmail.com>
2024-09-23 09:42:14 +0300

    Update neovim setup script
    - Fixed issues with output style
    - Fixed issue with installing node.js
    - Fixed issue with installing coc-clangd

--- nvim/nvim_setup.sh
+++ nvim/nvim_setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
-    
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+NODE_VERSION=22
+OK_MSG="\t[ ${GREEN}OK${NC} ]"
+
 detect_package_manager() {
   if command -v apt &> /dev/null; then
     echo "apt"
@@ -9,7 +15,7 @@ detect_package_manager() {
   elif command -v pacman &> /dev/null; then
     echo "pacman"
   else
-      echo "Unknown package manager (expected apt/dnf/pacman)"
+    echo "Unknown package manager (expected apt/dnf/pacman)"
     exit 1
   fi
 }
@@ -18,73 +24,75 @@ PACKAGE_MANAGER=$(detect_package_manager)
 
 install_package() {
   PACKAGE=$1
+  echo -n "Installing: $PACKAGE"
   case $PACKAGE_MANAGER in
     apt)
-      sudo apt update && sudo apt install -y $PACKAGE
+      sudo apt update -qq && sudo apt install -y $PACKAGE &> /dev/null
       ;;
     dnf)
-      sudo dnf install -y $PACKAGE
+      sudo dnf install -y $PACKAGE &> /dev/null
       ;;
     pacman)
-      sudo pacman -Sy --noconfirm $PACKAGE
+      sudo pacman -Sy --noconfirm $PACKAGE &> /dev/null
       ;;
   esac
+  echo -e $OK_MSG 
 }
 
 CONFIG_DIR=~/.config/nvim
 if [ ! -d "$CONFIG_DIR" ]; then
   mkdir -p "$CONFIG_DIR"
 elif [ -f $CONFIG_DIR/init.vim ]; then
-    echo "Remove ~/.config/nvim/init.vim and try again"
-    exit 1
+  echo -e "${RED}Remove ~/.config/nvim/init.vim and try again${NC}"
+  exit 1
 fi
 
+sudo echo none &> /dev/null
+
 if ! command -v nvim &> /dev/null; then
-    echo "Installing neovim..."
     install_package "neovim"
 fi
 
 if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-  echo "Installing vim-plug..."
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > dev/null
-else
-  echo "vim-plug already installed"
+  echo -n "Installing: vim-plug"
+  curl -s -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  echo -e $OK_MSG 
 fi
 
-# Node.js for coc.nvim
 if ! command -v node &> /dev/null; then
-  echo "Installing Node.js..."
-  # Node Version Manager
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  nvm install 22 > dev/null
-else
-  echo "Node.js already installed"
+  echo -n "Installing: nvm"
+  curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash &> /dev/null
+  export NVM_DIR="$HOME/.nvm" &> /dev/null
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &> /dev/null
+  echo -e $OK_MSG 
+
+
+  echo -n "Installing: node$NODE_VERSION"
+  nvm install $NODE_VERSION &> /dev/null
+  nvm use $NODE_VERSION &> /dev/null
+  echo -e $OK_MSG 
 fi
 
 if ! command -v clang-format &> /dev/null; then
-  echo "Installing clang-format..."
-  #install_package "clang-format" > dev/null
-else
-  echo "clang-format already installed"
+  echo -n "Installing: clang-format"
+  #install_package "clang-format"
+  echo -e $OK_MSG 
 fi
 
-if command -v nvim &> /dev/null && command -v node &> /dev/null; then
-  echo "All dependeces are installed"
-else
-    echo "Some issue during dependeces instalation"
+if ! command -v nvim &> /dev/null || ! command -v node &> /dev/null; then
+    echo -e "${RED}Some issue during dependeces instalation. Try to restart terminal and run script again.${NC}"
     exit 1
 fi
 
-echo "Copying ./init.vim to ~/.config/nvim/init.vim"
+echo -n "Copying ./init.vim to ~/.config/nvim/init.vim"
 cp ./init.vim $CONFIG_DIR/init.vim
+echo -e $OK_MSG 
 
-echo "Installing Neovim plugins..."
-nvim --headless +PlugInstall +qall > /dev/null
+echo -n "Installing: plugins"
+nvim --headless +PlugInstall +qall &> /dev/null
+nvim --headless +"CocInstall -sync coc-clangd" +qall &> /dev/null
+nvim --headless +"CocCommand clangd.install" +qall &> /dev/null
+echo -e $OK_MSG 
 
-echo "Installing coc-clangd extension..."
-nvim --headless +"CocInstall -sync coc-clangd" +qall > /dev/null
-
-echo "Setup finised"
+echo -e "${GREEN}Setup finised${NC}"
