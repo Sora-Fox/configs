5f6aea3c51d0614d2e0cfe8213f4020a57c910d4
fox <dhhugtu2@gmail.com>
2025-01-05 14:42:24 +0300

    Update zsh: remove rpromt, new aliases, CXX env vars

--- zsh/.zshenv
+++ zsh/.zshenv
@@ -10,5 +10,14 @@ export HISTSIZE=10000
 export SAVEHIST=0
 
 export EDITOR=nvim
-export CXX=clang++
+export TERM=xterm-256color
 
+export CXX=clang++
+export CC=clang
+export CXXFLAGS="-O2 -march=native -mtune=native -Wall -Wextra -Wpedantic -stdlib=libc++"
+export CFLAGS="-O2 -Wall -Wextra -Wpedantic -march=native"
+
+#unset WAYLAND_DISPLAY
+#export DISPLAY=:0
+#export GDK_BACKEND=x11
+#export XDG_SESSION_TYPE=x11
--- zsh/promt.zsh
+++ zsh/promt.zsh
@@ -1,11 +1,18 @@
-git_prompt_status() {
-  local CYAN="%F{cyan}"
-  local SAND="%F{183}"
+custom_prompt() {
+  local RED="%F{red}"
   local YELLOW="%F{220}"
-  local RESET="%f"
-  local SEPARATOR="%F{white} • ${RESET}"
+  local SAND="%F{183}"
+  local GREEN="%F{green}"
+  local CYAN="%F{cyan}"
+  local BROWN="%F{yellow}"
 
-  local git_info=""
+  local RESET="%f"
+  local SEPARATOR="%F{white} :: ${RESET}"
+
+  PROMPT=${RED}%n${RESET}
+  PROMPT+=${SEPARATOR}
+  PROMPT+=${BROWN}%~${RESET}
+
   local branch=$(git branch --show-current 2>/dev/null)
   if [[ -n "$branch" ]]; then
       local modified=$(git diff --name-only                      2>/dev/null | wc -l)
@@ -13,28 +20,16 @@ git_prompt_status() {
       local staged=$(git diff --cached --name-only               2>/dev/null | wc -l)
       local to_push=$(git log origin/$branch..$branch --oneline  2>/dev/null | wc -l)
 
-      git_info+=${CYAN}$branch${RESET}
-      ((untracked > 0)) && git_info+="${SEPARATOR}${YELLOW}U~${untracked}${RESET}"
-      ((modified > 0))  && git_info+="${SEPARATOR}${ORANGE}M~${modified}${RESET}"
-      ((staged > 0))    && git_info+="${SEPARATOR}${SAND}S~${staged}${RESET}"
-      ((to_push > 0))   && git_info+="${SEPARATOR}${SAND}P~${to_push}${RESET}"
+      PROMPT+=${SEPARATOR}
+      PROMPT+=${CYAN}$branch${RESET}
+      ((untracked > 0)) && PROMPT+="${SEPARATOR}${YELLOW}U~${untracked}${RESET}"
+      ((modified > 0))  && PROMPT+="${SEPARATOR}${ORANGE}M~${modified}${RESET}"
+      ((staged > 0))    && PROMPT+="${SEPARATOR}${SAND}S~${staged}${RESET}"
+      ((to_push > 0))   && PROMPT+="${SEPARATOR}${SAND}P~${to_push}${RESET}"
   fi
-  echo "$git_info"
-}
 
-custom_prompt() {
-  local RED="%F{red}"
-  local GREEN="%F{green}"
-  local BROWN="%F{yellow}"
-  local RESET="%f"
-  local SEPARATOR="${LIGHT_GRAY} • ${RESET}"
-
-  PROMPT=${RED}%n${RESET}
-  PROMPT+=${SEPARATOR}
-  PROMPT+=${BROWN}%~${RESET}
   PROMPT+=$'\n'
   PROMPT+="${GREEN}>${RESET} "
-  RPROMPT=$(git_prompt_status)
 }
 
 autoload -Uz add-zsh-hook
